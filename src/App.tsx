e74308f7e0b81f90cbfeb7f7cc1799bb969f7155
yundevingV <91381230+yundevingV@users.noreply.github.com>
2024-08-14 16:38:53 +0900

    Feat : tailwind 적용기

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './tailwind.css'; // Tailwind CSS 파일 임포트
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <button 
+        className={`w-15 h-5 bg-yellow-300`}>
+          태일윈드
+        </button>
     </div>
   );
 }
